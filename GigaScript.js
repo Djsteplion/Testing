2a378179b59f0519c8d0b26004c3cfe11ff16d92
Stephen Olayiwola <djsteplion@gmail.com>
2023-07-31 00:12:55 +0100

    Add files via upload

---
+++ GigaScript.js
@@ -0,0 +1,11 @@
+/** Begininng Of script for menu **/
+
+function openNav(){
+    document.getElementById("sidenav").style.width="40%";
+}
+
+function closeNav(){
+    document.getElementById("sidenav").style.width="0";
+}
+
+/** End Of script for menu */
